9eb14b1c862a8dadc47b61b4d0ebc9aee683535c
T N Thanoof Naina <thanoof.tn@experionglobal.local>
2023-01-17 11:30:13 +0530

    CC5

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+Console.Write("Enter the number of fares, in odd, that need to be entered: ");
+int oddFares = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the number of fares, in even, that need to be entered: ");
+int evenFares = Convert.ToInt32(Console.ReadLine());
+int n = oddFares + evenFares;
+
+double[] fares = new double[n];
+double singleFare = 0;
+int odd = 0;
+int even = 1;
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Enter the Fare Charge: ");
+    singleFare = Convert.ToDouble(Console.ReadLine());
+    if (singleFare % 2 != 0)
+    {
+        fares[odd] = singleFare;
+        odd+=2;
+    }
+    else
+    {
+        fares[even] = singleFare;
+        even+=2;
+    }
+}
+foreach (double fare in fares)
+{
+    Console.WriteLine(fare);
+}
+
+
